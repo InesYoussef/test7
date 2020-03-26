d9dce3f72c02f054411d431c16f0f73f89b53637
ines1234yOussef <inesyoussef0707@gmail.com>
2020-03-26 19:57:25 +0100

    first commit

---
+++ Maxime/Helloworld/Block/Hello.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Maxime\Helloworld\Block;
+class Hello extends \Magento\Framework\View\Element\Template
+{
+}
---
+++ Maxime/Helloworld/Controller/Say/Hello.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/*on a chargé un layout */
+namespace Maxime\Helloworld\Controller\Say;
+class Hello extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        $this->_view->loadLayout();
+        $this->_view->getLayout()->initMessages();
+        $this->_view->renderLayout();
+    }
+}
